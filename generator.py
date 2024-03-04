f3154c082917e6b7f574be2595c11a072e237195
YanisC <yanis.calvez@lundimatin.fr>
2024-03-04 13:55:22 +0100

    Fichiers de créations du tableau de probabilities et génération des mots

---
+++ generator.py
@@ -0,0 +1,23 @@
+import json, random
+
+with open("ressources/liste_probabilites_fr.json", 'r', encoding='utf-8') as fichier:
+    letters_proba = json.load(fichier)
+
+def generer_mot(probabilities_list):
+    word = ""
+    current_char = "#"
+    while current_char != "_":
+        next_chars_list = probabilities_list[current_char]
+        if next_chars_list:
+            next_chars = next_chars_list.keys()
+            probas = next_chars_list.values()
+        else:
+            next_chars, probas = ['_'], [1]
+        current_char = random.choices(list(next_chars), weights=list(probas))[0]
+        if current_char != "_":
+            word += current_char
+    return word
+
+for i in range(10):
+    print(generer_mot(letters_proba))
+
---
+++ probabilities.py
@@ -0,0 +1,32 @@
+import json
+
+letters_proba = {}
+
+def checkChars(word):
+    formated_word = "#" + word + "_"
+    for i in range (len(formated_word) - 1):
+        char = formated_word[i]
+        next_char = formated_word[i+1];
+        if letters_proba.get(char) is None:
+            letters_proba[char] = {}
+        if letters_proba[char].get(next_char) is None:
+            letters_proba[char][next_char] = 0
+        letters_proba[char][next_char] += 1
+            
+
+file = open('ressources/liste_mots_fr_gutenberg_336k.txt', 'r', encoding='utf-8')
+wordlist = file.readlines()
+
+for line in wordlist:
+    word = line.strip().lower()
+    checkChars(word)
+
+for letter_key, letter in letters_proba.items():
+    total_letter = sum(letter.values())
+    for key, next_letter in letter.items():
+        letter[key] = next_letter / total_letter
+
+# Convert and write JSON object to file
+with open("ressources/liste_probabilites_fr.json", "w", encoding='utf8') as outfile: 
+    json.dump(letters_proba, outfile, ensure_ascii=False)
+print("Dump done to liste_probabilites_fr.json")
