ece1b8e56b51b3c14e2e61b81ad6173af8c06062
YanisC <yanis.calvez@lundimatin.fr>
2024-03-05 14:31:11 +0100

    Tableau de probas à 2 profondeur

--- generator.py
+++ generator.py
@@ -3,27 +3,12 @@ import json, random
 with open("ressources/liste_probabilites_fr.json", 'r', encoding='utf-8') as fichier:
     letters_proba = json.load(fichier)
 
-def testChar(test_char, word):
-    if test_char == "_" and len(word) < 2:
-        #Si le mot fait moins de 2 caractères on relance
-        return False
-    
-    if len(word) > 2 and (test_char == word[-1] == word[-2]):
-        #Si on a 3 fois le même caractère on relance
-        return False
-    
-    if len(word) == 1 and (test_char == word[0]):
-        #Si les deux premiers caractères sont identiques on relance
-        return False
-
-    return True
-
-
 def generer_mot(probabilities_list):
-    word = ""
+    word = "##"
     current_char = "#"
     while current_char != "_":
-        next_chars_list = probabilities_list[current_char]
+        before_char = word[-2]
+        next_chars_list = probabilities_list[before_char][current_char]
         if next_chars_list:
             next_chars = next_chars_list.keys()
             probas = next_chars_list.values()
@@ -31,19 +16,12 @@ def generer_mot(probabilities_list):
             next_chars, probas = ['_'], [1]
         
 
-        test_char = random.choices(list(next_chars), weights=list(probas))[0]
-
-        test_status = testChar(test_char, word)
-
-        if not test_status:
-            continue
-
-        current_char = test_char
+        current_char = random.choices(list(next_chars), weights=list(probas))[0]
         
         if current_char != "_":
             word += current_char
         
-    return word
+    return (word[2:]).capitalize()
 
 for i in range(100):
     print(generer_mot(letters_proba))
--- probabilities.py
+++ probabilities.py
@@ -3,15 +3,18 @@ import json
 letters_proba = {}
 
 def checkChars(word):
-    formated_word = "#" + word + "_"
-    for i in range (len(formated_word) - 1):
+    formated_word = "##" + word + "_"
+    for i in range (len(formated_word) - 2):
         char = formated_word[i]
         next_char = formated_word[i+1];
+        next_next_char = formated_word[i+2];
         if letters_proba.get(char) is None:
             letters_proba[char] = {}
         if letters_proba[char].get(next_char) is None:
-            letters_proba[char][next_char] = 0
-        letters_proba[char][next_char] += 1
+            letters_proba[char][next_char] = {}
+        if letters_proba[char][next_char].get(next_next_char) is None:
+            letters_proba[char][next_char][next_next_char] = 0
+        letters_proba[char][next_char][next_next_char] += 1
             
 
 file = open('ressources/liste_mots_fr_gutenberg_336k.txt', 'r', encoding='utf-8')
@@ -21,10 +24,11 @@ for line in wordlist:
     word = line.strip().lower()
     checkChars(word)
 
-for letter_key, letter in letters_proba.items():
-    total_letter = sum(letter.values())
-    for key, next_letter in letter.items():
-        letter[key] = next_letter / total_letter
+for before_letter_key, before_letter in letters_proba.items():
+    for letter_key, letter in before_letter.items():
+        total_letter = sum(letter.values())
+        for key, next_letter in letter.items():
+            letter[key] = next_letter / total_letter
 
 # Convert and write JSON object to file
 with open("ressources/liste_probabilites_fr.json", "w", encoding='utf8') as outfile: 
